6cf35efb96cc70481900ee7142a9d7bf0ad7eb68
samdobson28 <134799184+samdobson28@users.noreply.github.com>
2025-01-13 21:36:29 -0500

    api keys, without the actual keys, which are in .env

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,12 @@
-# Example settings
-API_KEY = "your-api-key"
-API_SECRET = "your-api-secret"
-BASE_CURRENCY = "BTC"
-QUOTE_CURRENCY = "USDT"
+# settings.py
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+PAPER_TRADING = True
+API_KEY = os.getenv("ALPACA_API_KEY")
+API_SECRET = os.getenv("ALPACA_API_SECRET")
+ALPACA_BASE_URL = os.getenv("ALPACA_BASE_URL")
